7819c80b7ba283de6624d62484c953a689966d3d
hjjhhd <2447364650@qq.com>
2026-07-13 12:19:44 +0800

    LED

--- hub.py
+++ hub.py
@@ -1,1 +1,20 @@
-print("a")
+from pyb import UART
+
+
+# Stores the most recently received UART1 data.
+uart1_read = None
+
+
+class uart_hub:
+    def __init__(self, baudrate=9600):
+        # UART1: TX=PB6 (X9), RX=PB7 (X10).
+        self.uart1 = UART(1, baudrate)
+
+    def uart1_read(self):
+        """Read pending UART1 data and save it in the uart1_read variable."""
+        global uart1_read
+
+        if self.uart1.any():
+            uart1_read = self.uart1.read()
+
+        return uart1_read
--- main.py
+++ main.py
@@ -1,1 +1,17 @@
-# main.py -- put your code here!
+import time
+
+from pyb import LED
+
+
+led = LED(3)
+
+
+def blink_led():
+    """Flash the red onboard LED once per second."""
+    while True:
+        led.toggle()
+        time.sleep_ms(900)
+
+
+if __name__ == "__main__":
+    blink_led()
